92761ff83c0f169c0ee7473985a2bf8737994cb3
thenoobchad <noobchaddy@gmail.com>
2026-01-28 23:31:02 +0100

    fix:classe1s

--- app/teacher/students/[studentId]/grades/page.tsx
+++ app/teacher/students/[studentId]/grades/page.tsx
@@ -1,6 +1,6 @@
 import { db } from "@/database/drizzzle"
 import { students } from "@/database/schema"
-import { rankStudentsGrades } from "@/lib/actions"
+
 import { eq, } from "drizzle-orm"
 
 export default async function GradesCard({ params }: { params: Promise<{ studentId: string }> }) {
@@ -8,8 +8,6 @@ export default async function GradesCard({ params }: { params: Promise<{ student
     const { studentId } = await params
     const [student] = await db.select({ studentAdmissionNumber: students.admissionNumber, studentName: students.firstName }).from(students).where(eq(students.id, studentId))
 
-    const result = await rankStudentsGrades(studentId)
-    
     return (
         <div>Details for: {student.studentName} ({student.studentAdmissionNumber})
 
