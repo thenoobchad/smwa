d93bb1fbdb37e5ed12e0c090dba3549fc94585d9
thenoobchad <noobchaddy@gmail.com>
2026-01-28 06:17:02 +0100

    fix:hero image size

--- app/teacher/students/[studentId]/report/page.tsx
+++ app/teacher/students/[studentId]/report/page.tsx
@@ -1,11 +1,11 @@
-import { getScoreSheetData } from "@/lib/actions"
+
 
 
 export default async function ReportCard({ params }: { params: Promise<{ studentId: string }> }) {
     
     const { studentId } = await params
     
-    const data = await getScoreSheetData(studentId, "2024/2025")
+   
   return (
     <div>page</div>
   )
