68cca857aef2ad0d25233e47d29a43be2441f4ff
thenoobchad <noobchaddy@gmail.com>
2026-01-31 08:09:57 +0100

    fix:metric view1

--- components/print-button.tsx
+++ components/print-button.tsx
@@ -2,6 +2,8 @@
 
 export const PrintButton = () => {
   return (
-      <button onClick={() => window.print()} className="print:hidden mb-6 bg-black text-white px-4 py-2">Print Report Card</button>
+    <button
+      // onClick={() => window.print()}
+      className="print:hidden mb-6 bg-black text-white px-4 py-2">Print Report Card</button>
   )
 }
