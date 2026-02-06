36c801f073f63f0d2d2b1bb15bd63aae815043d8
thenoobchad <noobchaddy@gmail.com>
2026-02-06 10:32:36 +0100

    fix:sonner alert

--- app/dashboard/students/[studentId]/scores/page.tsx
+++ app/dashboard/students/[studentId]/scores/page.tsx
@@ -9,18 +9,20 @@ import { eq } from "drizzle-orm"
 
 export default async function StudentScore({ params }: {params: Promise<{ studentId: string }> }) {
   const { studentId } = await  params
+  const session = await getActiveSession()
+  if (!session || session.length === 0) return
 
-  const [session] = await getActiveSession()
+  const activeSession = session[0]
   const [student] = await db.select().from(students).where(eq(students.id, studentId))
 
-  const gradesData = await getStudentGrades(studentId, session.term)
+  const gradesData = await getStudentGrades(studentId, activeSession.term)
   return (
 		<div>
 			<header>
 				<h1>
-					Entry for <span className="capitalize">{session.term}</span> Term
+					Entry for <span className="capitalize">{activeSession.term}</span> Term
         </h1>
-        <p>Session: <span>{session.name}</span></p>
+        <p>Session: <span>{activeSession.name}</span></p>
         <h4 className="py-2 capitalize">Student: {student.lastName} {student.firstName}</h4>
       </header>
       
